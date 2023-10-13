8e4364c478bc29ed6bd67fe03f4d4f48c8b27162
RazvanAVESALON <89531556+RazvanAVESALON@users.noreply.github.com>
2023-10-13 11:00:49 +0300

    LightningModule Finish

--- lighting_train.py
+++ lighting_train.py
@@ -1,9 +1,12 @@
+import torch.nn as nn
 import torch
-import torch.nn.functional as F
 import lightning as L
 
+# from torcheval.metrics import MeanSquaredError
+from torchmetrics import MeanSquaredError
 
-class LitAutoEncoder(L.LightningModule):
+
+class LitAngio(L.LightningModule):
     def __init__(self, network, opt_ch, lr):
         super().__init__()
         self.network = network
@@ -25,9 +28,21 @@ class LitAutoEncoder(L.LightningModule):
         output = self.network(inputs)
         output = output.to("cuda")
         print(output.type, targets.type)
-        loss = F.mse_loss(output, targets)
+        criterion = nn.MSELoss().to("cuda")
+        loss = criterion(output, targets)
+        mse_metric = MeanSquaredError().to("cuda")
+        mse_train = mse_metric(output, targets)
+
         self.log(
-            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True,
+        )
+        self.log(
+            "train_mse",
+            mse_train,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
         )
         return loss
 
@@ -35,7 +50,12 @@ class LitAutoEncoder(L.LightningModule):
         inputs, target, idx = valid_batch
         output = self.network(inputs)
         output = output.to("cuda")
-        loss_val = F.mse_loss(output, target)
+        print(output.shape, target.shape)
+        criterion = nn.MSELoss().to("cuda")
+        loss_val = criterion(output, target)
+        mse_metric = MeanSquaredError().to("cuda")
+        mse_val = mse_metric(output, target)
         print(output.shape, target.shape)
         self.log("val_loss", loss_val)
+        self.log("mse_loss", mse_val)
         return loss_val
--- lstm_train.py
+++ lstm_train.py
@@ -9,37 +9,40 @@ import torchmetrics
 from datetime import datetime
 from angio_class import AngioClass
 from comet_ml import Experiment
-from torchsummary import summary 
+from torchsummary import summary
 from cnn_lstm import CNNLSTM
-import albumentations as A 
-from lighting_train import LitAutoEncoder
+import albumentations as A
+from lighting_train import LitAngio
 import lightning as L
-#from cnn_lstm_rshp import CNNLSTM
+
+
+# from cnn_lstm_rshp import CNNLSTM
 def main():
     print(f"pyTorch version {torch.__version__}")
     print(f"torchvision version {torchvision.__version__}")
     print(f"torchmetrics version {torchmetrics.__version__}")
     print(f"CUDA available {torch.cuda.is_available()}")
-    
+
     config = None
-    with open('config.yaml') as f: 
+    with open("config.yaml") as f:
         config = yaml.safe_load(f)
-    
+
     experiment = Experiment(
         api_key="wwQKu3dl9l1bRZOpeKs0y3r8S",
         project_name="general",
-        workspace="razvanavesalon",)
+        workspace="razvanavesalon",
+    )
 
     exp_name = f"Experiment_Dice_index{datetime.now().strftime('%m%d%Y_%H%M')}"
 
-    exp_path="D:\\Angio\\ANGIO-LSTM\\Experimente"
-    exp_path = pt.Path(exp_path,exp_name)
+    exp_path = "D:\\Angio\\ANGIO-LSTM\\Experimente"
+    exp_path = pt.Path(exp_path, exp_name)
     exp_path.mkdir(exist_ok=True)
     dir = "Weights"
-    path = pt.Path(exp_path)/dir
+    path = pt.Path(exp_path) / dir
     path.mkdir(exist_ok=True)
 
-    network=CNNLSTM(num_classes=2)
+    network = CNNLSTM(num_classes=2)
     summary(network)
     experiment.log_parameters(config)
 
@@ -48,38 +51,53 @@ def main():
     f.write(yml_data)
     f.close()
 
-    pixels = T.Compose([
+    pixels = T.Compose(
+        [
+            TR.ToTensord(keys="img"),
+        ]
+    )
+    geometric_t = A.Compose(
+        [
+            A.Resize(
+                height=config["data"]["img_size"][0],
+                width=config["data"]["img_size"][1],
+            )
+        ],
+        keypoint_params=A.KeypointParams(format="yx", remove_invisible=False),
+    )
 
-        TR.ToTensord(keys="img"),
-    ])
-    geometric_t= A.Compose([
-        A.Resize(height=config['data']['img_size'][0] , width=config['data']['img_size'][1])
-    ],keypoint_params=A.KeypointParams(format='yx', remove_invisible=False))
+    dataset_df = pd.read_csv(config["data"]["dataset_csv"])
 
-    dataset_df = pd.read_csv(config['data']['dataset_csv'])
+    train_df = dataset_df.loc[dataset_df["subset"] == "train", :]
+    train_ds = AngioClass(
+        train_df, img_size=config["data"]["img_size"], geometrics_transforms=geometric_t
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_ds, batch_size=config["train"]["bs"], shuffle=True, drop_last=True
+    )
 
-    train_df = dataset_df.loc[dataset_df["subset"] == "train",:]
-    train_ds = AngioClass(train_df, img_size=config['data']['img_size'],geometrics_transforms=geometric_t)
-    train_loader = torch.utils.data.DataLoader(train_ds, batch_size=config['train']['bs'], shuffle=True,drop_last=True)
-    
     valid_df = dataset_df.loc[dataset_df["subset"] == "valid", :]
     print(valid_df)
-    valid_ds = AngioClass(valid_df, img_size=config['data']['img_size'],geometrics_transforms=geometric_t)
-    valid_loader = torch.utils.data.DataLoader(valid_ds, batch_size=config['train']['bs'],shuffle=False,drop_last=True)
-    if config['train']['opt'] == 'Adam':
-        opt = torch.optim.Adam(network.parameters(), lr=config['train']['lr'])
-    elif config['train']['opt'] == 'SGD':
-        opt = torch.optim.SGD(network.parameters(), lr=config['train']['lr'])
-    elif config['train']['opt'] == "RMSprop":
-        opt = torch.optim.RMSprop(
-            network.parameters(), lr=config['train']['lr'])
-    
-  
-    
-    trainer = L.Trainer(max_steps=100,accelerator='gpu')
-    trainer.fit(LitAutoEncoder(network,config['train']['opt'],config['train']['lr']), train_loader,valid_loader)
-   
+    valid_ds = AngioClass(
+        valid_df, img_size=config["data"]["img_size"], geometrics_transforms=geometric_t
+    )
+    valid_loader = torch.utils.data.DataLoader(
+        valid_ds, batch_size=config["train"]["bs"], shuffle=False, drop_last=True
+    )
+    if config["train"]["opt"] == "Adam":
+        opt = torch.optim.Adam(network.parameters(), lr=config["train"]["lr"])
+    elif config["train"]["opt"] == "SGD":
+        opt = torch.optim.SGD(network.parameters(), lr=config["train"]["lr"])
+    elif config["train"]["opt"] == "RMSprop":
+        opt = torch.optim.RMSprop(network.parameters(), lr=config["train"]["lr"])
+
+    trainer = L.Trainer(max_steps=100, accelerator="gpu")
+    trainer.fit(
+        LitAngio(network, config["train"]["opt"], config["train"]["lr"]),
+        train_loader,
+        valid_loader,
+    )
+
 
 if __name__ == "__main__":
     main()
-
